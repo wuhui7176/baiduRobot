ce107999ef2d89a06701b8dacc3700f97258b0bd
wuhui7176@qq.com <xxx5211314>
2017-11-08 22:01:53 +0800

    message

--- public/javascripts/casperjs/login/2.js
+++ public/javascripts/casperjs/login/2.js
@@ -22,17 +22,17 @@ var casper = require('casper').create({
         console.log('onDie: ' + msg);
     },
     onAlert: function (msg) {//alert的回调函数
-        // console.log("onAlert===========================msg:" + msg);
     }
 });
 casper.start();
 var fs = require('fs')
 var data = fs.read("17080310848.txt");
 var cookieJson = JSON.parse(data)
-var cookie ;
+var cookie ="" ;
 for (var i in cookieJson) {
     cookie  = cookie + cookieJson[i].name +"="+ cookieJson[i].value+";"
 }
+
 var url = 'https://www.baidu.com/';
 //var url = 'https://tieba.baidu.com/index.html';
 //登录百度
@@ -46,20 +46,23 @@ casper.thenOpen(url,{
     }
 })
 
+// casper.then(function() {
+//     this.fill('form[action="/s"]', { wd: 'thoughtworks' }, true);//填入form，进行搜索
+// });
+
 casper.then(function(){
-    this.click("#u1 a[href='http://www.baidu.com']");
-    // this.click("#u1 a.mnav[]");
-    this.wait(300);
+    // this.click(".s-isindex-wrap.s-sp-menu a[href='http://news.baidu.com']")
+    // this.click("#s_menu_mine .mine-text")
+    this.click("#u1 a[href='http://tieba.baidu.com']");
+    this.wait(1000);
 });
 
 casper.then(function(){
-    this.echo(cookieJson)
     this.echo('cookie::::::::::' + cookie);
 });
 
 casper.then(function(){
     var cookies = this.page.cookies;
-    // this.echo("cookie.length = " + cookies.length);
     for (var i in cookies) {
         this.echo(cookies[i].name + "=" +  cookies[i].value);
     }
